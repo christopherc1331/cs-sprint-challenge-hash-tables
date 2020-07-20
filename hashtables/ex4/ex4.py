9d486e7b0eeb9f0a171466bb33f87f9ba1e8c71f
Chris Cruz <christopherc1331@gmail.com>
2020-07-19 20:05:58 -0700

    Finished ex4.py

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,7 +4,23 @@ def has_negatives(a):
     """
     # Your code here
 
-    return result
+    my_dict = {}
+    my_list = []
+
+    for i in range(0,len(a)):
+        my_dict[a[i]] = i
+        
+
+    for i in range(0,len(a)):
+        if a[i] > 0:
+            # print("neg value here |",a[i] * -1)
+            # print("my_dict which will be checked against |",my_dict)
+            if (a[i] * -1) in my_dict:
+                # print("appending now")
+                my_list.append(a[i])
+
+
+    return my_list
 
 
 if __name__ == "__main__":
