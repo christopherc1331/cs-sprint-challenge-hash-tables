6b8b95708743bf22a2a2bd6fa4cf33ac1292a815
Chris Cruz <christopherc1331@gmail.com>
2020-07-19 20:30:24 -0700

    Finished ex3.py

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,8 +3,27 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    arrays_count = len(arrays)
+    my_dict = {}
+    my_list = []
 
-    return result
+
+    for list in arrays:
+        for item in list:
+            if item in my_dict:
+                my_dict[item] += 1
+            else:
+                my_dict[item] = 1
+    
+    my_dict_list = my_dict.items()
+
+    for i in my_dict_list:
+        if i[1] == arrays_count:
+            my_list.append(i[0])
+
+    return my_list
+
+
 
 
 if __name__ == "__main__":
